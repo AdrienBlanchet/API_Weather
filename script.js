a3c2c4cfa7ac295ca9a09dd859b03318e9de4972
adrien <test@test.com>
2022-02-12 22:01:15 +0100

    Fin, projet complet ajout de 6 boutons de couleur rose, il manque le responsive à finir

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const input = document.getElementById("text");
 const form = document.querySelector("form");
 const list = document.querySelector(".list");
 const global = document.querySelector(".global_weather");
-const image = document.querySelector("img");
+const buttons = document.querySelectorAll(".button")
+const logos = document.querySelectorAll(".button >  img")
+
 
 let town;
 let date_time;
@@ -14,84 +16,107 @@ let humidity;
 let speed_wind;
 let global_weather;
 let icon;
+let unité;
+
+
 
 
 
 async function fetchWeather(town) {
-    await fetch(`http://api.openweathermap.org/data/2.5/find?q=${town}&units=metric&lang=fr&limit=2&appid=ad833c311d7223b6bcf85c641143f86e`)
+    await fetch(`http://api.openweathermap.org/data/2.5/find?q=${town}&units=metric&lang=fr&appid=ad833c311d7223b6bcf85c641143f86e`)
       .then((res) => res.json())
       .then((data) => {
         
         if (data.cod === '200' && data.count > 0){
 
           console.log(data);
-          data.list.map((obj) => {
+          
 
-            if (obj.sys.country === 'FR' && obj.name.toLowerCase() === town){
-                  date_time = timestampConvert(data.list[0].dt); 
-                  cloud = obj.clouds.all;
-                  temperature = obj.main.temp;
-                  temp_feeling = obj.main.feels_like;
-                  pression_hpa = obj.main.pressure;
-                  humidity = obj.main.humidity;
-                  speed_wind = obj.wind.speed;
-                  global_weather = obj.weather[0].description;
-                  icon = obj.weather[0].icon
-                  console.log(icon);
-            }
-          })
+          let filterTab = [];
+          filterTab = data.list.filter((obj) => obj.sys.country === 'FR' && obj.name.toLowerCase() === town)
+          filterTab.forEach((obj) => {
+
+            date_time = timestampConvert(data.list[0].dt); 
+            cloud = obj.clouds.all;
+            temperature = obj.main.temp;
+            temp_feeling = obj.main.feels_like;
+            pression_hpa = obj.main.pressure;
+            humidity = obj.main.humidity;
+            speed_wind = obj.wind.speed;
+            global_weather = obj.weather[0].description;
+            icon = obj.weather[0].icon
+            
+
+            afficher();
+
+          }
+         )
         }else alert('Erreur saisir de nouveau');
       });
 } 
 
 
+
 let afficher = () => {
 
   global.innerHTML = `<h2>Météo de la ville de ${town} <br> ${date_time}</h2>
-                      <p>${global_weather}</p>`
-  list.innerHTML = `
-                    <ul>
-                      <li>Temperature : ${temperature}  °C</li>
-                      <li>Temperature ressentie : ${temp_feeling}  °C</li>
-                      <li>Pression atmosphétique : ${pression_hpa}  hPa</li>
-                      <li>Humidité : ${humidity}  %</li>
-                      <li>vent : ${speed_wind}  m/s</li>
-                    </ul>
-                    `
+                      <p>${global_weather}</p>
+                      <img class="icon_weather">`
+  
+  const image = document.querySelector(".icon_weather");
+  
   switch (icon) {
       case '01n' :
-       image.setAttribute('src', 'http://openweathermap.org/img/wn/01d@2x.png');
-      break;
-      case '02n':
-        image.setAttribute('src', 'http://openweathermap.org/img/wn/01d@2x.png');
-      break;
-      case '03n':
+      case '01d' :
         image.setAttribute('src', 'http://openweathermap.org/img/wn/01d@2x.png');
         break;
+      case '02n':
+      case '02d' :
+        image.setAttribute('src', 'http://openweathermap.org/img/wn/02d@2x.png');
+        break;
+      case '03n':
+      case '03d' :
+        image.setAttribute('src', 'http://openweathermap.org/img/wn/03d@2x.png');
+        break;
       case '04n':
-        image.setAttribute('src', 'http://openweathermap.org/img/wn/01d@2x.png');
+      case '04d' :
+        image.setAttribute('src', 'http://openweathermap.org/img/wn/04d@2x.png');
       break;
       case '09n':
-        image.setAttribute('src', 'http://openweathermap.org/img/wn/01d@2x.png');
+      case '09d' :
+        image.setAttribute('src', 'http://openweathermap.org/img/wn/09d@2x.png');
       break;
       case '10n':
-        image.setAttribute('src', 'http://openweathermap.org/img/wn/01d@2x.png');
+      case '10d' :
+        image.setAttribute('src', 'http://openweathermap.org/img/wn/10d@2x.png');
       break;
       case '11n':
-        image.setAttribute('src', 'http://openweathermap.org/img/wn/01d@2x.png');
+      case '11d' :
+        image.setAttribute('src', 'http://openweathermap.org/img/wn/11d@2x.png');
       break;
       case '13n':
-        image.setAttribute('src', 'http://openweathermap.org/img/wn/01d@2x.png');
+      case '13d' :
+        image.setAttribute('src', 'http://openweathermap.org/img/wn/13d@2x.png');
       break;
       case '50n':
-        image.setAttribute('src', 'http://openweathermap.org/img/wn/01d@2x.png');
+      case '50d' :
+        image.setAttribute('src', 'http://openweathermap.org/img/wn/50d@2x.png');
       break;
       default:
   }
 }
+///////////////////////////Fonction qui afficher les info aux clic des boutons//////////////////////////////////////////////////////////////////////////////
 
-        
-  
+let affiche_clic = (clic) => {
+  if (clic.match("u") != null) {                // match() retourne un tableau en précisant la position de la lettre passé en paramètre dans la chaine de caratère
+    alert("Veuillez entrer le nom d'un ville")  // si la condition est différent de null cela veut dire que clic contient une valeur précise et non "undefined"
+  }else{
+    list.innerHTML = `${clic}`
+
+  }
+}
+
+
 ///////Convertion du temps timestamp (nbre de seconde depuis 1970) au format jj/mm/aaaa hh:mm. code source du web web//////////////////
 let timestampConvert = (unixTimestamp) => {
   var date = new Date(unixTimestamp*1000);
@@ -102,6 +127,9 @@ let timestampConvert = (unixTimestamp) => {
             ":"+date.getMinutes()+
             ":"+date.getSeconds();
 }
+ 
+        
+  
 
 ///////////////////////////////////Mise en forme de l'input type text////////////////////////////
 
@@ -110,7 +138,7 @@ let transform = (city_lower) => {
   lower = city_lower.toLowerCase();
   if (lower.indexOf(" ") !== -1){
     while (lower.indexOf(" ") !== -1){
-        lower = lower.replace(" ", "+");
+        lower = lower.replace(" ", "-");
     }
     } 
   return lower;
@@ -124,6 +152,56 @@ input.addEventListener("input", (e) => {
 form.addEventListener("submit", async (e) => {
     e.preventDefault();
     await fetchWeather(town);
-    await afficher();
+    
     
   });
+
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    
+    console.log(e.target.className);
+
+    switch (e.target.className) {
+      case 'button button_T':
+      case 'icon_T' :
+       unité = " °C" ;
+       affiche_clic(temperature + unité);
+      break;
+      case 'button button_Tf':
+      case 'icon_Tf' :
+        unité = " °C" ;
+       affiche_clic(temp_feeling + unité);
+      break;
+      case 'button button_W':
+      case 'icon_W' :
+        unité = " m/s" ;
+       affiche_clic(speed_wind + unité);
+      break;
+      case 'button button_P':
+      case 'icon_P' :
+        unité = " hPa" ;
+       affiche_clic(pression_hpa + unité);
+      break;
+      case 'button button_H':
+      case 'icon_H' :
+        unité = " %" ;
+       affiche_clic(humidity + unité);
+      break;
+      case 'button button_C':
+      case 'icon_C' :
+        unité = " %" ;
+       affiche_clic(cloud + unité);
+      break;
+      default:
+      break;
+    }
+  })
+})
+
+
+    
+    
+    
+
+
